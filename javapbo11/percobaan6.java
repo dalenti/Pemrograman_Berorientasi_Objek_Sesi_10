42415219ee700afdc00b20d2c6aa0d9c54c3db73
LAPTOP-1OTEMH15\LENOVO <kdalenti@gmail.com>
2023-02-06 17:23:52 +0700

    no message

---
+++ javapbo11/percobaan6.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapbo11;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class percobaan6 {
+     static void demo(){
+        NullPointerException t;
+        t = new NullPointerException("Coba Throw");
+        throw t;
+        // Baris ini tidak lagi dikerjakan;
+        //System.out.println("Ini tidak lagi dicetak");
+    }
+    public static void main(String[] args) {
+        try{
+            demo();
+            System.out.println("Selesai");
+        }
+        catch(NullPointerException e){
+            System.out.println("Ada pesan error: "+ e);
+        }
+    }
+}
